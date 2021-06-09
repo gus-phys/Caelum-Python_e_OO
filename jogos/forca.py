21ecba22e2c660ccfd5efc1301881c05f7e7840c
Luis Silva <luisgustavot.silva@gmail.com>
2021-06-09 00:01:04 -0300

    Fix forca.py

--- jogos/forca.py
+++ jogos/forca.py
@@ -9,29 +9,31 @@ acertou = False
 enforcou = False
 erros = 0
 
-while(not acertou and not enforcou):
-    chute = input('Qual letra?')
+print(letras_acertadas)
 
-    posicao = 0
-    for letra in palavra_secreta:
-        acertou = '_' not in letras_acertadas
-        enforcou = erros == 6
-        if (chute.upper() == letra.upper()):
-            letras_acertadas[posicao] = letra
-        posicao += 1
-    print(letras_acertadas)
+while(not acertou and not enforcou):
+
+    chute = input('Qual letra?')
 
     if(chute in palavra_secreta):
         posicao = 0
         for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                letras_acertadas[posica] = letra
+            # acertou = '_' not in letras_acertadas
+            # enforcou = erros == 6
+            if (chute.upper() == letra.upper()):
+                # print(f"Encontrei a letra{letra} na posição {posicao}")
+                letras_acertadas[posicao] = letra
             posicao += 1
     else:
         erros += 1
 
-    while(not enforcou and not acertou):
-        chute = input('Qual a letra?')
+    acertou = '_' not in letras_acertadas
+    enforcou = erros == 5
+    print(letras_acertadas)
+
+if(acertou):
+    print('Você ganhou!!!')
+else:
+    print('Você perdeu!!!')
 
 print('Fim do jogo')
-#11
