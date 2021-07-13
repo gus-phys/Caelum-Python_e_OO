83a8db358a14ed8900aa19a49a9c80f235d0de29
LUIS GUSTAVO TOMAZ SILVA <luis.tomaz@sistemafiep.org.br>
2021-07-13 14:31:48 -0300

    Add args/kwargs script and menu module

---
+++ args_kwargs.py
@@ -0,0 +1,6 @@
+
+def teste_args_kwargs(arg, *args):
+    print('primeiro argumento: {}'.format(arg))
+    for arg in args:
+        print('outro argumetn: {}'.format(arg))
+        
--- jogos/forca.py
+++ jogos/forca.py
@@ -1,39 +1,40 @@
-print('******************************')
-print('***Bem vindo ao jogo da Forca!')
-print('******************************')
+def jogar():
+    print('******************************')
+    print('***Bem vindo ao jogo da Forca!')
+    print('******************************')
 
-palavra_secreta = 'banana'
-letras_acertadas = ['_', '_', '_', '_', '_', '_']
+    palavra_secreta = 'banana'
+    letras_acertadas = ['_', '_', '_', '_', '_', '_']
 
-acertou = False
-enforcou = False
-erros = 0
+    acertou = False
+    enforcou = False
+    erros = 0
 
-print(letras_acertadas)
-
-while(not acertou and not enforcou):
-
-    chute = input('Qual letra?')
-
-    if(chute in palavra_secreta):
-        posicao = 0
-        for letra in palavra_secreta:
-            # acertou = '_' not in letras_acertadas
-            # enforcou = erros == 6
-            if (chute.upper() == letra.upper()):
-                # print(f"Encontrei a letra{letra} na posição {posicao}")
-                letras_acertadas[posicao] = letra
-            posicao += 1
-    else:
-        erros += 1
-
-    acertou = '_' not in letras_acertadas
-    enforcou = erros == 5
     print(letras_acertadas)
 
-if(acertou):
-    print('Você ganhou!!!')
-else:
-    print('Você perdeu!!!')
+    while(not acertou and not enforcou):
 
-print('Fim do jogo')
+        chute = input('Qual letra?')
+
+        if(chute in palavra_secreta):
+            posicao = 0
+            for letra in palavra_secreta:
+                # acertou = '_' not in letras_acertadas
+                # enforcou = erros == 6
+                if (chute.upper() == letra.upper()):
+                    # print(f"Encontrei a letra{letra} na posição {posicao}")
+                    letras_acertadas[posicao] = letra
+                posicao += 1
+        else:
+            erros += 1
+
+        acertou = '_' not in letras_acertadas
+        enforcou = erros == 5
+        print(letras_acertadas)
+
+    if(acertou):
+        print('Você ganhou!!!')
+    else:
+        print('Você perdeu!!!')
+
+    print('Fim do jogo')
