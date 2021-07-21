ea4bca877229cece3fecc641d96379b7b49f0d39
LUIS GUSTAVO TOMAZ SILVA <luis.tomaz@sistemafiep.org.br>
2021-07-21 16:51:43 -0300

    Encapsulate adivinhacao.py

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -1,28 +1,65 @@
-def jogar():
+import random
+
+numero_min = 0
+numero_max = 100
+def imprime_mensagem_abertura():
     print('***********************')
     print('* JOGO DA ADIVINHAÇÃO *')
     print('***********************')
+    print('\n')
+    print('Tente advinhar o número, de {} a {}'.format(numero_min, numero_max))
 
-    numero_secreto = 42
-    total_de_tentativas = 3
+def carrega_numero_secreto(numero_min, numero_max):
+    numero = random.randrange(numero_min, numero_max + 1)
+
+    return numero
+
+def pede_nivel(nivel):
+    if(nivel==1):
+        total_de_tentativas = 20
+        print('Nível {} escolhido, você tem {} tentativas.'.format(nivel,
+         total_de_tentativas))
+    elif(nivel==2):
+        total_de_tentativas = 10
+        print('Nível {} escolhido, você tem {} tentativas.'.format(nivel,
+         total_de_tentativas))
+    elif(nivel==3):
+        total_de_tentativas = 5
+        print('Nível {} escolhido, você tem {} tentativas.'.format(nivel,
+         total_de_tentativas))
+
+    return total_de_tentativas
+
+def jogar():
+    imprime_mensagem_abertura()
+
+    numero_secreto = carrega_numero_secreto(numero_min, numero_max)
+    total_de_pontos = 1000
+
+    nivel = int(input('Escolha o nível de dificuldade de 1 a 3: '))
+    total_de_tentativas = pede_nivel(nivel)
 
     for rodada in range(1, total_de_tentativas + 1):
         print('Tentativa {} de {}'.format(rodada, total_de_tentativas))
 
         chute = int(input('Digite o seu palpite: '))
-        print('Seu palpite é: ', chute)
+        # print('Seu palpite é: ', chute)
 
         acertou = chute == numero_secreto
         maior = chute > numero_secreto
         menor = chute < numero_secreto
 
-
-        if(acertou):
-          print('Você acertou!')
-          break
-        elif(maior):
-          print('Você errou! O número inserido é maior que o número secreto.')
+        if(maior):
+            diferenca = chute - numero_secreto
+            total_de_pontos = total_de_pontos - diferenca
+            print('Você errou! O número inserido é maior que o número secreto.')
         elif(menor):
-          print('Você errou! O número inserido é menor que o número secreto.')
+            diferenca = numero_secreto - chute
+            total_de_pontos = total_de_pontos - diferenca
+            print('Você errou! O número inserido é menor que o número secreto.')
+        elif(acertou):
+            print('Você acertou!')
+            break
 
-    print('Fim do jogo')
+    print('Fim do jogo!!!\nSua pontuação foi de {} pontos'.format(
+    total_de_pontos))
