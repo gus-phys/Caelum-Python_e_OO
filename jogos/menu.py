c064a4490b6c7b1f9132eb9c5707178a76e72671
LUIS GUSTAVO TOMAZ SILVA <luis.tomaz@sistemafiep.org.br>
2021-07-21 13:00:03 -0300

    Encapsulate forca.py

--- jogos/menu.py
+++ jogos/menu.py
@@ -1,13 +1,17 @@
 import adivinhacao
 import forca
+import adivinhacao_opcional
 
 print('*********************************')
 print('**********MENU DE JOGOS**********')
 print('*********************************')
 print("1. Adivinhação")
 print("2. Forca")
+print("3. Adivinhacão (nível)")
 escolha = int(input("Qual jogo quer jogar? Digite o número: "))
 if escolha == 1:
     adivinhacao.jogar()
 elif escolha == 2:
     forca.jogar()
+elif escolha == 3:
+    adivinhacao_opcional.jogar()
